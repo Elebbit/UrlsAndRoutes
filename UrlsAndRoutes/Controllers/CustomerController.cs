60a8dabb3f376952fa72d616eb7693c9a5c1085a
De.Elebit <de.elebit@gmail.com>
2019-04-26 14:39:28 +0900

    ~495 : Below Targeting Other Controllers

---
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index() => View("Result",
+            new Result { Controller = nameof(CustomerController), Action = nameof(Index)});
+
+        public ViewResult List(string id)
+        {
+            var r = new Result {Controller = nameof(CustomerController), Action = nameof(List)};
+
+            r.Data["id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
+    } 
+}
